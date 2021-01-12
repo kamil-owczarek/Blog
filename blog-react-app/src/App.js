203808efbb27f6671415864b574b3310e4a67b3b
Kamil Owczarek <owczarek9721@gmail.com>
2021-01-12 17:54:28 +0100

    Created main page and components, configured axios

--- blog-react-app/src/App.js
+++ blog-react-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Link, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/posts" className="navbar-brand">
+            Blog
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/posts"} className="nav-link">
+                Posts
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/"} className="nav-link">
+                Contact
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "posts"]} component={PostsList} />
+            <Route exact path="/add" component={AddPost} />
+            <Route exact path="/post/:id" component={Post} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
