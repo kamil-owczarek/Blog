43c04ef6c28cafcf11995127f7934d760aba0e2c
Kamil Owczarek <owczarek9721@gmail.com>
2021-01-12 22:38:33 +0100

    Added contact form and refactored code

--- blog-react-app/src/App.js
+++ blog-react-app/src/App.js
@@ -4,44 +4,46 @@ import { Link, Route, Switch } from "react-router-dom";
 import PostsList from "../src/components/posts-list.component";
 import AddPost from "../src/components/add-post.component";
 import Post from "../src/components/post.component";
+import ContactForm from "../src/ContactForm"
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/" className="navbar-brand">
-            Blog
-          </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/posts"} className="nav-link">
-                Posts
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add post
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/contact"} className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </div>
-        </nav>
+    render() {
+        return (
+        <div>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/" className="navbar-brand">
+                Blog
+            </a>
+            <div className="navbar-nav mr-auto">
+                <li className="nav-item">
+                <Link to={"/posts"} className="nav-link">
+                    Posts
+                </Link>
+                </li>
+                <li className="nav-item">
+                <Link to={"/add"} className="nav-link">
+                    Add post
+                </Link>
+                </li>
+                <li className="nav-item">
+                <Link to={"/contact"} className="nav-link">
+                    Contact
+                </Link>
+                </li>
+            </div>
+            </nav>
 
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path="/posts" component={PostsList} />
-            <Route exact path="/add" component={AddPost} />
-            <Route exact path="/posts/:id" component={Post} />
-          </Switch>
+            <div className="container mt-3">
+            <Switch>
+                <Route exact path="/posts" component={PostsList} />
+                <Route exact path="/add" component={AddPost} />
+                <Route exact path="/posts/:id" component={Post} />
+                <Route exact path="/contact" component={ContactForm} />
+            </Switch>
+            </div>
         </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
