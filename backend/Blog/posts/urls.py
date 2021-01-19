a3d88156894aafb7ce88893cdeb773411a3c195f
Kamil Owczarek <owczarek9721@gmail.com>
2021-01-19 21:33:37 +0100

    Added view tests

--- backend/Blog/posts/urls.py
+++ backend/Blog/posts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from posts import views 
 
 urlpatterns = [ 
-    url(r'^api/posts$', views.post_list),
-    url(r'^api/posts/(?P<pk>[0-9]+)$', views.post_detail)
+    url(r'^api/posts$', views.post_list, name="get_post_delete_posts"),
+    url(r'^api/posts/(?P<pk>[0-9]+)$', views.post_detail, name="get_put_delete_post")
 ]
